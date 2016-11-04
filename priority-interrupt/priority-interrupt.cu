bdf7d9ea9b143751d489141ac4442e6c9fcefff5
Vance Miller <vance@cs.utexas.edu>
2016-11-04 16:42:24 -0500

    refactoring priority-interrupt

--- priority-interrupt/priority-interrupt.cu
+++ priority-interrupt/priority-interrupt.cu
@@ -18,7 +18,7 @@
 #include <cuda_profiler_api.h>
 #include <helper_cuda.h>
 
-#define TOTAL_SIZE  16 * 1024*1024
+#define TOTAL_SIZE  256*1024*1024
 
 // # threadblocks
 #define TBLOCKS 1024
@@ -35,15 +35,15 @@
   exit(-1);}} while(0)
 
 // experiment iterations to compute averages
-#define N_ITERATIONS 1024
-#define N_HIGH_KERNELS 1
+#define N_ITERATIONS 2
+#define N_HIGH_KERNELS 4
 
 // copy from source -> destination arrays
 __device__ void slow_kernel(int *dst, int *src, size_t n) {
   int num = gridDim.x * blockDim.x;
   int id = blockDim.x * blockIdx.x + threadIdx.x;
   for (int i = id; i < n / sizeof(int); i += num) {
-#define DELAY 1024
+#define DELAY 2048
     for (volatile int j = 0; j < DELAY; j++)
       ;
     dst[i] = src[i];
@@ -83,7 +83,7 @@ void mem_init(int *buf, size_t n) {
 }
 
 // Forward declaration
-int preempt_stream(int, int);
+cudaError_t experiment(int priority_low, int priority_hi);
 
 int main(int argc, char **argv) {
   cudaDeviceProp device_prop;
@@ -99,11 +99,11 @@ int main(int argc, char **argv) {
     fprintf(stderr,
         "%s requires Compute Capability of SM 3.5 or higher to run.\nexiting...\n",
         argv[0]);
-    exit(EXIT_WAIVED);
+    exit (EXIT_WAIVED);
   }
 
   // get the range of priorities available
-  // [ greatest_priority, lowest_priority ]
+  // [ greatest_priority, least_priority ]
   int priority_low;
   int priority_hi;
   checkCudaErrors(
@@ -111,33 +111,13 @@ int main(int argc, char **argv) {
   printf("CUDA stream priority range: LOW: %d to HIGH: %d\n", priority_low,
       priority_hi);
 
-  preempt_stream(priority_low, priority_hi);
+  experiment(priority_low, priority_hi);
 
-  exit(EXIT_SUCCESS);
+  exit (EXIT_SUCCESS);
 }
 
-/**
- * Creates a stream with low priority and starts a long-running kernel on it.
- * Creates a stream with high priority and runs a short-running kernel on it,
- * after the low-priority kernel has begun.
- * -- If preemption works, the run time of the low priority kernel should
- *    be extended by the runtime of the high priority kernel which preempts it.
- */
-int preempt_stream(int priority_low, int priority_hi) {
-  // Create streams
-  size_t n_streams = 2; // Two streams (low and high)
-                        // let index 0 hold low and 1 hold high
-  cudaStream_t streams[n_streams];
-  checkCudaErrors(
-      cudaStreamCreateWithPriority(&streams[0], cudaStreamNonBlocking,
-          priority_low));
-  checkCudaErrors(
-      cudaStreamCreateWithPriority(&streams[1], cudaStreamNonBlocking,
-          priority_hi));
-
-  size_t size;
-  size = TOTAL_SIZE;
-
+cudaError_t solo_test(cudaStream_t* streams, int low_priority_stream_idx,
+    int high_priority_stream_idx, size_t n_streams, size_t size) {
   // Initialize host data
   int *h_src[n_streams];
   for (int i = 0; i < n_streams; i++) {
@@ -157,7 +137,7 @@ int preempt_stream(int priority_low, int priority_hi) {
   for (int i = 0; i < n_streams; i++) {
     checkCudaErrors(cudaMalloc(&d_src[i], size));
     checkCudaErrors(
-        cudaMemcpy(d_src[i], h_src[i], size, cudaMemcpyHostToDevice));
+        cudaMemcpyAsync(d_src[i], h_src[i], size, cudaMemcpyHostToDevice, streams[i]));
   }
 
   // allocate memory for memcopy destination
@@ -166,117 +146,177 @@ int preempt_stream(int priority_low, int priority_hi) {
     checkCudaErrors(cudaMalloc(&d_dst[i], size));
   }
 
-  /* */
+  for (int i = 0; i < n_streams; i++) {
+    checkCudaErrors(cudaStreamSynchronize(streams[i]));
+  }
+  /* Kernel invocations */
 
-  // Begin profiling
-  checkCudaErrors(cudaProfilerStart());
-
-  // Time low priority on its own
-  double low_ms;
-  for (int iteration = 0; iteration < N_ITERATIONS; iteration++) {
-    float ms;
+  // Run each priority on its own
+  for (int i = 0; i < n_streams; i++) {
     cudaEvent_t start, end;
     checkCudaErrors(cudaEventCreate(&start));
     checkCudaErrors(cudaEventCreate(&end));
-    checkCudaErrors(cudaEventRecord(start, streams[0]));
-    low<<<TBLOCKS, THREADS, 0, streams[0]>>>(d_dst[0], d_src[0], TOTAL_SIZE);
-    checkCudaErrors(cudaEventRecord(end, streams[0]));
+    checkCudaErrors(cudaEventRecord(start, streams[i]));
+    if (i == low_priority_stream_idx) {
+      low<<<TBLOCKS, THREADS, 0, streams[i]>>>(d_dst[i], d_src[i], TOTAL_SIZE);
+    } else {
+      high<<<TBLOCKS, THREADS, 0, streams[i]>>>(d_dst[i], d_src[i], TOTAL_SIZE);
+    }
+    checkCudaErrors(cudaEventRecord(end, streams[i]));
     checkCudaErrors(cudaEventSynchronize(end));
-
-    checkCudaErrors(cudaEventElapsedTime(&ms, start, end));
-    low_ms += ((double) ms) / ((double) N_ITERATIONS);
   }
-  printf("Low priority solo average elapsed time %0.6f ms\n", low_ms);
 
-  // Time high priority on its own
-  double high_ms;
-  for (int iteration = 0; iteration < N_ITERATIONS; iteration++) {
-    float ms;
-    cudaEvent_t start, end;
-    checkCudaErrors(cudaEventCreate(&start));
-    checkCudaErrors(cudaEventCreate(&end));
-    checkCudaErrors(cudaEventRecord(start, streams[1]));
-    high<<<TBLOCKS, THREADS, 0, streams[1]>>>(d_dst[1], d_src[1], TOTAL_SIZE);
-    checkCudaErrors(cudaEventRecord(end, streams[1]));
-    checkCudaErrors(cudaEventSynchronize(end));
-
-    checkCudaErrors(cudaEventElapsedTime(&ms, start, end));
-    high_ms += ((double) ms) / ((double) N_ITERATIONS);
-  }
-  printf("High priority solo average elapsed time %0.6f ms\n", high_ms);
-
-  // Start low priority then interrupt it with high priority
-  double low_preempt_ms;
-  double high_preempt_ms;
-
-  for (int iteration = 0; iteration < N_ITERATIONS; iteration++) {
-    // create some events
-    cudaEvent_t ev_start[1 + N_HIGH_KERNELS];
-    cudaEvent_t ev_end[1 + N_HIGH_KERNELS];
-    for (int i = 0; i < 1 + N_HIGH_KERNELS; i++) {
-      checkCudaErrors(cudaEventCreate(&ev_start[i]));
-      checkCudaErrors(cudaEventCreate(&ev_end[i]));
-    }
-
-    checkCudaErrors(cudaEventRecord(ev_start[0], streams[0]));
-    low_preempt<<<TBLOCKS, THREADS, 0, streams[0]>>>(d_dst[0], d_src[0],
-        TOTAL_SIZE);
-    checkCudaErrors(cudaEventRecord(ev_end[0], streams[0]));
-
-    // synchronize on the start, so we launch this after the low priority kernel has started
-
-    checkCudaErrors(cudaEventSynchronize(ev_start[0]));
-
-    for (int j = 0; j < N_HIGH_KERNELS; j++) {
-      checkCudaErrors(cudaEventRecord(ev_start[1 + j], streams[1]));
-      high_preempt<<<TBLOCKS, THREADS, 0, streams[1]>>>(d_dst[1], d_src[1],
-          TOTAL_SIZE);
-      checkCudaErrors(cudaEventRecord(ev_end[1 + j], streams[1]));
-
-      checkCudaErrors(cudaEventSynchronize(ev_end[1 + j]));
-
-    }
-
-    checkCudaErrors(cudaEventSynchronize(ev_end[0]));
-
-    float ms[1 + N_HIGH_KERNELS];
-    for (int i = 0; i < 1 + N_HIGH_KERNELS; i++) {
-      checkCudaErrors(cudaEventElapsedTime(&ms[i], ev_start[i], ev_end[i]));
-    }
-    low_preempt_ms += ((double) ms[0]) / ((double) N_ITERATIONS);
-    for (int i = 0; i < N_HIGH_KERNELS; i++) {
-      high_preempt_ms += ((double) ms[1 + i]) / ((double) N_ITERATIONS) / ((double) N_HIGH_KERNELS);
-    }
-
-  }
-  printf("Low priority preempted by high priority test\n");
-  printf("Low priority elapsed time %0.6f ms\n", low_preempt_ms);
-  printf("High priority elapsed time %0.6f ms\n", high_preempt_ms);
-  // these numbers aren't reliable. use nvprof to get better times
-  printf("Overhead of context switch %0.6f ms\n",
-      (low_preempt_ms - low_ms - high_ms) + (high_preempt_ms - high_ms));
-
-  // Stop profiling
-  checkCudaErrors(cudaProfilerStop());
-
-  /* */
-
-  size = TOTAL_SIZE;
+  // Copy result to host
   for (int i = 0; i < n_streams; i++) {
     checkCudaErrors(
-        cudaMemcpy(h_dst[i], d_dst[i], size, cudaMemcpyDeviceToHost));
+        cudaMemcpyAsync(h_dst[i], d_dst[i], size, cudaMemcpyDeviceToHost, streams[i]));
   }
 
-  // check results of kernels
-  for (int i = 0; i < n_streams; i++) {
-    ERR_NE(memcmp(h_dst[i], h_src[i], size), 0);
-  }
+//  // check results of kernels
+//  for (int i = 0; i < n_streams; i++) {
+//    ERR_NE(memcmp(h_dst[i], h_src[i], size), 0);
+//  }
 
   // Clean up
   for (int i = 0; i < n_streams; i++) {
     checkCudaErrors(cudaFree(d_src[i]));
     checkCudaErrors(cudaFree(d_dst[i]));
   }
+  for (int i = 0; i < n_streams; i++) {
+    free(h_src[i]);
+  }
+  return cudaSuccess;
+}
 
-  return 0;
+cudaError_t concurrent_test(cudaStream_t* streams, int low_priority_stream_idx,
+    int high_priority_stream_idx, int n_kernels, size_t size) {
+  // Initialize host data
+  int *h_src[n_kernels];
+  for (int i = 0; i < n_kernels; i++) {
+    ERR_EQ(h_src[i] = (int * ) malloc(size), NULL);
+    mem_init(h_src[i], size);
+  }
+
+  // Initialize device data
+  int *h_dst[n_kernels];
+  for (int i = 0; i < n_kernels; i++) {
+    ERR_EQ(h_dst[i] = (int * ) malloc(size), NULL);
+    memset(h_dst[i], 0, size);
+  }
+
+  // copy source data -> device
+  int *d_src[n_kernels];
+  checkCudaErrors(cudaMalloc(&d_src[0], size));
+  checkCudaErrors(
+      cudaMemcpyAsync(d_src[0], h_src[0], size, cudaMemcpyHostToDevice, streams[low_priority_stream_idx]));
+
+  for (int i = 0; i < n_kernels; i++) {
+    checkCudaErrors(cudaMalloc(&d_src[i], size));
+    checkCudaErrors(
+        cudaMemcpyAsync(d_src[i], h_src[i], size, cudaMemcpyHostToDevice, streams[high_priority_stream_idx]));
+  }
+
+  // allocate memory for memcopy destination
+  int *d_dst[n_kernels];
+  for (int i = 0; i < n_kernels; i++) {
+    checkCudaErrors(cudaMalloc(&d_dst[i], size));
+  }
+
+  /* */
+
+  // create some events
+  cudaEvent_t ev_start[n_kernels];
+  cudaEvent_t ev_end[n_kernels];
+  for (int i = 0; i < n_kernels; i++) {
+    checkCudaErrors(cudaEventCreate(&ev_start[i]));
+    checkCudaErrors(cudaEventCreate(&ev_end[i]));
+  }
+  
+  for (int i = 0; i < 2; i++) {
+    checkCudaErrors(cudaStreamSynchronize(streams[i]));
+  }
+
+  // Start low priority kernel
+  checkCudaErrors(
+      cudaEventRecord(ev_start[0], streams[low_priority_stream_idx]));
+  low_preempt<<<TBLOCKS, THREADS, 0, streams[0]>>>(d_dst[0], d_src[0],
+      size);
+  checkCudaErrors(cudaEventRecord(ev_end[0], streams[low_priority_stream_idx]));
+
+  // synchronize on the start, so we launch this after the low priority kernel has started
+  checkCudaErrors(cudaEventSynchronize(ev_start[0]));
+
+  // Launch n_kernels - 1 high priority kernels synchronously 
+  for (int i = 1; i < n_kernels; i++) {
+    checkCudaErrors(
+        cudaEventRecord(ev_start[i], streams[high_priority_stream_idx]));
+    high_preempt<<<TBLOCKS, THREADS, 0, streams[high_priority_stream_idx]>>>(d_dst[i], d_src[i],
+        size);
+    checkCudaErrors(
+        cudaEventRecord(ev_end[i], streams[high_priority_stream_idx]));
+    checkCudaErrors(cudaEventSynchronize(ev_end[i]));
+  }
+
+  // wait for the low priority kernel to finish
+  checkCudaErrors(cudaEventSynchronize(ev_end[0]));
+
+  // Copy result to host
+  checkCudaErrors(
+      cudaMemcpyAsync(h_dst[0], d_dst[0], size, cudaMemcpyDeviceToHost, streams[low_priority_stream_idx]));
+
+  for (int i = 1; i < n_kernels; i++) {
+    checkCudaErrors(
+        cudaMemcpyAsync(h_dst[i], d_dst[i], size, cudaMemcpyDeviceToHost, streams[high_priority_stream_idx]));
+  }
+
+//  // check results of kernels
+//  for (int i = 0; i < n_kernels; i++) {
+//    ERR_NE(memcmp(h_dst[i], h_src[i], size), 0);
+//  }
+
+  // Clean up
+  for (int i = 0; i < n_kernels; i++) {
+    checkCudaErrors(cudaFree(d_src[i]));
+    checkCudaErrors(cudaFree(d_dst[i]));
+  }
+  for (int i = 0; i < n_kernels; i++) {
+    free(h_src[i]);
+  }
+  return cudaSuccess;
+}
+
+/**
+ * Creates streams with priority ranging from high to low and stores them in the streams array.
+ * Streams are ordered from highest to lowest priority. 
+ */
+cudaError_t createStreams(cudaStream_t* streams, int priority_low,
+    int priority_hi, size_t n_streams) {
+  for (int i = 0; i < n_streams; i++) {
+    checkCudaErrors(
+        cudaStreamCreateWithPriority(&streams[i], cudaStreamNonBlocking,
+            priority_hi + i));
+  }
+  return cudaSuccess;
+}
+
+/**
+ * Creates a stream with low priority and starts a long-running kernel on it.
+ * Creates a stream with high priority and runs a short-running kernel on it,
+ * after the low-priority kernel has begun.
+ * -- If preemption works, the run time of the low priority kernel should
+ *    be extended by the runtime of the high priority kernel which preempts it.
+ */
+cudaError_t experiment(int priority_low, int priority_hi) {
+  // Create streams
+  size_t n_streams = (priority_low - priority_hi) + 1;
+  cudaStream_t streams[n_streams];
+  checkCudaErrors(createStreams(streams, priority_low, priority_hi, n_streams));
+  size_t size = TOTAL_SIZE; // Size of host data
+  size_t n_kernels = N_HIGH_KERNELS + 1; // 1 low and N high
+
+  for (int i = 0; i < N_ITERATIONS; i++) {
+    checkCudaErrors(solo_test(streams, 0, 1, n_streams, size));
+    checkCudaErrors(concurrent_test(streams, 0, 1, n_kernels, size));
+  }
+  return cudaSuccess;
 }
